3bb9773e31f4d23697da4e7548d0ab28906a2684
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-09 11:39:30 +0600

    mongodb connections successfully

---
+++ app/api/route.js
@@ -0,0 +1,15 @@
+import connectDB from "@/lib/connectDB";
+import Ticket from "../(models)/ticket";
+import { NextResponse } from "next/server";
+
+export async function POST(req) { 
+  try {
+    const body = await req.json();
+    const ticketData = body.formData();
+    await Ticket.create(ticketData);
+
+    return NextResponse.json({ message: 'Ticket created successfully' }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500})
+  }
+}
