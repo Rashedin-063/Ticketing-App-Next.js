3f18780ca1720b4749ebc9380c8d8d1efa000f39
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-09 13:56:09 +0600

    api created to get all the tickets from db

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
 import TicketCard from "./(components)/TicketCard";
 
-export default function Dashboard() {
+
+const getTickets = async () => {
+ try {
+   const res = await fetch('http://localhost:3000/api/tickets');
+   return res.json();
+ } catch (error) {
+  console.error('Failed to get tickets', error);
+ }
+}
+
+export default async function Dashboard() {
+  
+  const {tickets} = await getTickets();
+
   return (
     <div className='p-5'>
       <div className='lg:grid grid-cols-2 xl:grid-cols-4 '>
