b837afd4692cf1f59a53e530ff5457f841fccef6
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-24 21:49:06 +0600

    readme file updated

---
+++ app/api/[id]/route.js
@@ -0,0 +1,12 @@
+import Ticket from "@/app/(models)/ticket";
+import { NextResponse } from "next/server";
+
+
+export async function DELETE(req, { params }) {
+  const { id } = await params;
+  try {
+    
+  } catch (error) {
+    
+  }
+}
