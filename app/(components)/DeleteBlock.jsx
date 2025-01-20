74b47e466576a7f712d1ddf9b48f59681065d363
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-20 19:31:21 +0600

    delete functionality implemented

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,5 +1,30 @@
+"use client"
+import { useRouter } from 'next/navigation';
 import { FaX } from 'react-icons/fa6';
-const DeleteBlock = () => {
-  return <FaX  className='text-red-400 hover:cursor-pointer hover:text-red-200'/>;
+
+
+const DeleteBlock = (params) => {
+  console.log(params)
+  console.log('hello')
+  
+
+  const  {id}  = params;
+  console.log('the id is', id)
+  
+  const router = useRouter()
+ 
+  const deleteTicket = async () => {
+   
+    
+   
+    const res = await fetch(`http://localhost:3000/api/${id}`, {
+      method: 'DELETE',
+    });
+    if (res.ok) {
+      router.refresh()
+    }
+}
+
+  return <FaX onClick={() => deleteTicket(id)}  className='text-red-400 hover:cursor-pointer hover:text-red-200'/>;
 };
 export default DeleteBlock;
--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -3,10 +3,11 @@ import { NextResponse } from "next/server";
 
 
 export async function DELETE(req, { params }) {
-  const { id } = await params;
+  const {id} = params;
   try {
-    
+    await Ticket.findByIdAndDelete(id);
+    return NextResponse.json({ message: 'Ticket deleted successfully' }, {status: 200});
   } catch (error) {
-    
+    return NextResponse.json({message: 'Error', error}, {status: 500})
   }
 }
