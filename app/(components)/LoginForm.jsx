ef018b4bdb7356e9deeb680f946a029070f8f2b6
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-23 18:04:16 +0600

    login form updated

---
+++ app/(components)/LoginForm.jsx
@@ -0,0 +1,15 @@
+
+
+const LoginForm = () => {
+  return (
+    <Form>
+      <button>
+        Sign In With Google
+      </button>
+      <button>
+        Sign In With Github
+      </button>
+    </Form>
+  )
+}
+export default LoginForm
